7adcbbc8229eb20ec52570608495017fbe9552e8
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-06 17:24:37 -0300

    Fanalizando salarioComJuros

--- 1051-salarioComJuros/Program.cs
+++ 1051-salarioComJuros/Program.cs
@@ -1,1 +1,22 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string nome = Console.ReadLine ();
+        double salarioFixo = double.Parse (Console.ReadLine ());
+        double vendas = double.Parse (Console.ReadLine ());
+
+        double comissao = vendas * 0.15;
+        double total = salarioFixo + comissao;
+
+        Console.WriteLine ("TOTAL = R$" + total.ToString("F2"));
+
+
+
+    }
+
+}
