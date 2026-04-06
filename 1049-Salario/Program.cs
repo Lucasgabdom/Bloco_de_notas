f13650ceb7c814caed604efd6b2f2a12f5379403
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-06 16:41:14 -0300

    Exercicio de salario finalizado

--- 1049-Salario/Program.cs
+++ 1049-Salario/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int funcionario = int.Parse(Console.ReadLine());
+        int horas = int.Parse(Console.ReadLine());
+        double valorHora = double.Parse(Console.ReadLine());
+
+        void calcularSalario()
+        {
+            double salario = horas * valorHora;
+            Console.WriteLine($"NUMBER = {funcionario}");
+            Console.WriteLine($"SALARY = U$ {salario:F2}");
+        }
+
+        calcularSalario();
+
+    }
+
+}
