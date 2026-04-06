229f75caebcd0fcc9dbe7fc4c5e51eaf9152e9a6
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-06 15:54:17 -0300

    Criando estrutura de pastas

---
+++ Salario.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace beencrowd
+{
+    internal class Salario
+    {
+        public void salario()
+        {
+            Console.WriteLine("Hellou, World!");
+        }
+
+    }
+}
+Remove - Item - Recurse - Force beencrowd
