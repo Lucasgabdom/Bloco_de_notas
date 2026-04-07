74bcc7aa3de23995e5ca0d17868ebf7a9ab8221a
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-07 13:48:46 -0300

    Exercício 1054 finalizado

--- 1053-esfera/Program.cs
+++ 1053-esfera/Program.cs
@@ -1,1 +1,21 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        double raio = float.Parse(Console.ReadLine());
+        double pi = 3.14159;
+        double volume = (4.0 / 3) * pi * Math.Pow(raio, 3);
+
+        Console.WriteLine("VOLUME = " + volume.ToString("F3"));
+
+        // no double volume o 4.0 é escrito com ponto para indicar que é um número de ponto flutuante, e não um inteiro. Isso é importante porque a divisão entre um inteiro e outro inteiro pode resultar em um número inteiro, o que não é o caso aqui, já que queremos um resultado de ponto flutuante. Portanto, ao usar 4.0, garantimos que a divisão seja realizada corretamente e o resultado seja um número de ponto flutuante, mesmo que o valor de pi seja uma constante.
+
+
+
+    }
+
+}
