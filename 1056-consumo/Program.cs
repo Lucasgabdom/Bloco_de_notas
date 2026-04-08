c371dde013ca9b6937b0c55c1bd07d7f7f80e568
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-08 08:11:26 -0300

    Exercício 1056 finalizado

--- 1056-consumo/Program.cs
+++ 1056-consumo/Program.cs
@@ -1,1 +1,16 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int distancia = int.Parse(Console.ReadLine());
+        double consumo = double.Parse(Console.ReadLine());
+
+
+
+    }
+
+}
