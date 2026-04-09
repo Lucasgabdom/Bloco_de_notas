decce4e265fc7615599b4c10fe03a70091db06ed
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-09 15:15:48 -0300

    Exercício número 1060 resolvido

--- 1059-gastoDeCombustiveis/Program.cs
+++ 1059-gastoDeCombustiveis/Program.cs
@@ -1,1 +1,18 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int horas = int.Parse(Console.ReadLine());
+        double VelocidadeMedia = double.Parse(Console.ReadLine());
+
+        double consumo = VelocidadeMedia * horas / 12;
+
+        Console.WriteLine(consumo.ToString("F3"));
+
+    }
+
+}
