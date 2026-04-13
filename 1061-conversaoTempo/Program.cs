e4f0d312bf0672c939dd58fc051668890c3e7d2d
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-13 17:17:29 -0300

    Exercício 1062 resolvido.

--- 1061-conversaoTempo/Program.cs
+++ 1061-conversaoTempo/Program.cs
@@ -1,1 +1,19 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        int N = int.Parse(Console.ReadLine());
+
+        int horas = N / 3600; //Quantas horas completas cabem dentro de N segundos.
+        int minutos = (N % 3600) / 60; //Os segundos restantes depois de retirar as horas completas.
+        int segundos = N % 60; // Os segundos restantes depois de retirar as horas completas e os minutos completos.
+
+        Console.WriteLine($"{horas}:{minutos}:{segundos}");
+
+    }
+
+}
