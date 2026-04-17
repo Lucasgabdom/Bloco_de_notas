b876868a02b54fe51050d72ddee8507d24a32891
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-17 10:23:54 -0300

    Exercício 1070 resolvido

--- 1069-coordenadasDeUmPonto/Program.cs
+++ 1069-coordenadasDeUmPonto/Program.cs
@@ -1,1 +1,45 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] valores = Console.ReadLine().Split(' ');
+
+        double x = double.Parse(valores[0]);
+        double y = double.Parse(valores[1]);
+
+        if (x == 0 && y == 0)
+        {
+            Console.WriteLine("Origem");
+        }
+        else if (x < 0 && y > 0)
+        {
+            Console.WriteLine("Q2");
+        }
+        else if (x > 0 && y > 0)
+        {
+            Console.WriteLine("Q1");
+        }
+        else if (x < 0 && y < 0)
+        {
+            Console.WriteLine("Q3");
+        }
+        else if (x > 0 && y < 0)
+        {
+            Console.WriteLine("Q4");
+        }
+        else if (x == 0)
+        {
+            Console.WriteLine("Eixo Y");
+        }
+        else if (y == 0)
+        {
+            Console.WriteLine("Eixo X");
+
+
+        }
+    }
+}
---
+++ 1070-sortSimples/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Data;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] entrada = Console.ReadLine().Split(' ');
+
+        int a = int.Parse(entrada[0]);
+        int b = int.Parse(entrada[1]);
+        int c = int.Parse(entrada[2]);
+
+        int[] ordenados = {a,b,c};
+
+        Array.Sort(ordenados);
+
+        foreach (int numero in ordenados)
+        {
+            Console.WriteLine(numero);
+        }
+        Console.WriteLine();
+
+        Console.WriteLine(a);
+        Console.WriteLine(b);
+        Console.WriteLine(c);
+    }
+
+}
