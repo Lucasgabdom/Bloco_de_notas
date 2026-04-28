bf0d096c912b3c399c2cbc4ed375c4cdee5400b3
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-28 13:23:13 -0300

    Exercício 1071 finalizado

---
+++ 1071-triangulo/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] entrada = Console.ReadLine().Split(' ');
+
+        double a = double.Parse(entrada[0]);
+        double b = double.Parse(entrada[1]);
+        double c = double.Parse(entrada[2]);
+
+    }
+
+}
