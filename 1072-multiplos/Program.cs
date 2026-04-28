4ba686a41ccc2aa733889097ee0f141aaf82a336
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-28 14:42:07 -0300

    Aprendendo sobre ifs e variavel bool

--- 1072-multiplos/Program.cs
+++ 1072-multiplos/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        String[] entrada = Console.ReadLine().Split(' ');
+
+        double a = double.Parse(entrada[0]);
+        double b = double.Parse(entrada[1]);
+
+        if (a % b == 0 || b % a == 0)
+        {
+            Console.WriteLine("Sao Multiplos");
+        }
+        else
+        {
+            Console.WriteLine("Nao sao Multiplos");
+        }
+
+    }
+
+}
---
+++ 1073-tiposDeTriangulo/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        String[] entrada = Console.ReadLine().Split(' ');
+
+        double[] lados =
+        {
+            double.Parse(entrada[0]),
+            double.Parse(entrada[1]),
+            double.Parse(entrada[2])
+        };
+
+        Array.Sort(lados);
+        Array.Reverse(lados);
+
+        double a = lados[0];
+        double b = lados[1];
+        double c = lados[2];
+
+        bool formaTriangulo = a < b + c;
+
+        
+        if (!formaTriangulo)
+        {
+            Console.WriteLine("NAO FORMA TRIANGULO");
+            return;
+        }
+
+       
+        if (a * a == b * b + c * c)
+        {
+            Console.WriteLine("TRIANGULO RETANGULO");
+        }
+        else if (a * a > b * b + c * c)
+        {
+            Console.WriteLine("TRIANGULO OBTUSANGULO");
+        }
+        else if (a * a < b * b + c * c)
+        {
+            Console.WriteLine("TRIANGULO ACUTANGULO");
+        }
+
+
+        if (formaTriangulo && a == b && b == c)
+        {
+            Console.WriteLine("TRIANGULO EQUILATERO");
+        }
+        else if (a == b || a == c || b == c)
+        {
+            Console.WriteLine("TRIANGULO ISOSCELES");
+        }
+    }
+}
