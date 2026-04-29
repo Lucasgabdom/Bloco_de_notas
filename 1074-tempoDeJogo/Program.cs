20fa4b6069f4971c2e72a87d07e616844726827c
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-04-29 16:40:02 -0300

    Aprendendo a testar outros modos

--- 1074-tempoDeJogo/Program.cs
+++ 1074-tempoDeJogo/Program.cs
@@ -1,1 +1,25 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string[] entrada = Console.ReadLine().Split(' ');
+
+        int horaInicial = int.Parse(entrada[0]);
+        int horaFinal = int.Parse(entrada[1]);
+
+        int calcularHora = horaFinal - horaInicial;
+
+        if (calcularHora > 0)
+        {
+            Console.WriteLine($"O JOGO DUROU {calcularHora} HORA(S)");
+        } else
+        {
+            Console.WriteLine($"O JOGO DUROU {24 + calcularHora} HORA(S)");
+        }
+    }
+
+}
