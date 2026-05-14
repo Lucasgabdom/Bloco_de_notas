ace80984228721f9bee92462193cc8cf7e4dff48
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-05-14 15:32:13 -0300

    Exercicio 1080 resolvido

--- 1079-carregaOuNao/Program.cs
+++ 1079-carregaOuNao/Program.cs
@@ -1,1 +1,24 @@
-﻿Console.WriteLine("Hello, World!");
+﻿using System;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+
+        string line;
+        while ((line = Console.ReadLine()) != null)
+        {
+            line = line.Trim();
+            if (line == "") continue;
+
+            string[] parts = line.Split(' ');
+            uint a = uint.Parse(parts[0]);
+            uint b = uint.Parse(parts[1]);
+
+            Console.WriteLine(a ^ b);
+        }
+
+    }
+
+}
---
+++ 1080-FlavioJosephus/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Runtime.Serialization.Formatters;
+
+class URI
+{
+
+    static void Main(string[] args)
+    {
+        int totalCasos = int.Parse(Console.ReadLine());
+
+        for (int caso = 1; caso <= totalCasos; caso++)
+        {
+            string[] entrada = Console.ReadLine().Split(' ');
+            int totalPessoas = int.Parse(entrada[0]);
+            int tamanhoSalto = int.Parse(entrada[1]);
+
+            // Monta o círculo
+            List<int> circulo = new List<int>();
+            for (int i = 1; i <= totalPessoas; i++)
+                circulo.Add(i);
+
+            // Simula as eliminações
+            int posicaoAtual = 0;
+            while (circulo.Count > 1)
+            {
+                posicaoAtual = (posicaoAtual + tamanhoSalto - 1) % circulo.Count;
+                circulo.RemoveAt(posicaoAtual);
+                posicaoAtual = posicaoAtual % circulo.Count;
+            }
+
+            Console.WriteLine($"Case {caso}: {circulo[0]}");
+        }
+
+
+    }
+
+}
