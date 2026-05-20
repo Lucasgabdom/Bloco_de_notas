9c3d90a21f881b346565e103d473051466e4870e
Lucas Gabriel <gabriellucaswbtrombone@gmail.com>
2026-05-20 14:06:32 -0300

    Exercicio imposto de renda finalizado

--- 1081-Energia/Program.cs
+++ 1081-Energia/Program.cs
@@ -1,17 +1,46 @@
 ﻿using System;
+using System.Collections.Generic;
 
 class URI
 {
-
-    static void Main(string[] args)
+    static int Josephus(int totalpessoas, int tamanhoSalto)
     {
+        List<int> circulo = new List<int>();
+        for (int i = 1; i <= totalpessoas; i++)
+            circulo.Add(i);
 
-        /**
-         * Escreva a sua solução aqui
-         * Code your solution here
-         * Escriba su solución aquí
-         */
+        circulo.RemoveAt(0);
+
+        int posicaoAtual = (tamanhoSalto - 1) % circulo.Count;
+
+        while (circulo.Count > 1)
+        {
+            circulo.RemoveAt(posicaoAtual);
+            posicaoAtual = (posicaoAtual + tamanhoSalto - 1) % circulo.Count;
+        }
+
+        return circulo[0];
 
     }
 
+    static void Main(string[] args)
+    {
+        string linha;
+        while ((linha = Console.ReadLine()) != null)
+        {
+            int n = int.Parse(linha);
+            if (n == 0) break;
+
+
+            int salto = 1;
+            while (true)
+            {
+                if (Josephus(n, salto) == 13)
+                    break;
+                salto++;
+            }
+
+            Console.WriteLine(salto);
+        }
+    }
 }
